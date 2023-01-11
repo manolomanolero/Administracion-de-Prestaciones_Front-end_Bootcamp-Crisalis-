9c96d03091abbd04da4f475d6cb4b10d59b9ab8e
MPautasso <manuelpautasso@gmail.com>
2023-01-11 07:38:36 -0300

    Added impuesto service

---
+++ mercado-crisalis/src/app/services/impuestos.service.ts
@@ -0,0 +1,32 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Impuesto } from '../pages/impuestos/impuesto.model';
+import baserUrl from './helper';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ImpuestosService {
+
+  constructor(private httpClient: HttpClient) { }
+
+  getImpuestos() {
+    return this.httpClient.get(`${baserUrl}/impuestos`);
+  }
+
+  saveImpuesto(impuesto: Impuesto) {
+    return this.httpClient.post(`${baserUrl}/impuestos`, impuesto);
+  }
+
+  updateImpuesto(impuesto: Impuesto) {
+    return this.httpClient.put(`${baserUrl}/impuestos`, impuesto);
+  }
+
+  findImpuesto(id:number) {
+    return this.httpClient.get(`${baserUrl}/impuestos/search?id=` + id);
+  }
+
+  deleteImpuesto(id: number) {
+    return this.httpClient.delete(`${baserUrl}/impuestos?id=`+ id);
+  }
+}
