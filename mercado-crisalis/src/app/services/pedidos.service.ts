98f22c21c81f09bac2f3aba73ac9ec2c0ddd75c6
mpautasso <manuelpautasso@gmail.com>
2023-01-23 07:57:04 -0300

    Added Pedidos service

---
+++ mercado-crisalis/src/app/services/pedidos.service.ts
@@ -0,0 +1,32 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Pedido } from '../models/pedido.model';
+import baserUrl from './helper';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PedidosService {
+
+  constructor(private httpClient:HttpClient) { }
+
+  getPedidos() {
+    return this.httpClient.get(`${baserUrl}/pedidos`);
+  }
+
+  savePedido(pedido: Pedido) {
+    return this.httpClient.post(`${baserUrl}/pedidos`, pedido);
+  }
+
+  updatePedido(pedido: Pedido) {
+    return this.httpClient.put(`${baserUrl}/pedidos`, pedido);
+  }
+
+  findPedido(id:number) {
+    return this.httpClient.get(`${baserUrl}/pedidos/search?id=` + id);
+  }
+
+  deletePedido(id: number) {
+    return this.httpClient.delete(`${baserUrl}/pedidos?id=`+ id);
+  }
+}
